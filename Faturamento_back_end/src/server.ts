668754fdd9e258a09dd979c783e0cf9614dd2cba
Estevam Alcântara Ponte <46602186+EstevamPonte@users.noreply.github.com>
2022-07-31 18:10:06 -0300

    heroku deploy 4

--- Faturamento_back_end/src/server.ts
+++ Faturamento_back_end/src/server.ts
@@ -3,7 +3,6 @@ dotenv.config()
 import 'reflect-metadata'
 import express, {Request, Response, NextFunction} from 'express'
 import 'express-async-errors'
-import { Client } from 'pg'
 
 import cors from "cors"
 import { router } from './routes'
@@ -15,15 +14,6 @@ app.use(cors())
 
 app.use(express.json())
 app.use(router)
-
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
-
-client.connect();
 
 app.use((err: Error, req: Request, response: Response, next: NextFunction) => {
   if(err instanceof Error) {
